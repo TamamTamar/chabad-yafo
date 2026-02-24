55ae3463c50dcf1ff51c48891079d851c2cd2bb2
TamamTamar <Tamar.tamam770@gmail.com>
2026-02-24 20:23:22 +0200

    small v

--- client/src/components/FloatingSocials/FloatingSocials.tsx
+++ client/src/components/FloatingSocials/FloatingSocials.tsx
@@ -1,4 +1,4 @@
-import { Facebook, MessageCircle } from "lucide-react";
+import { Facebook, MessageCircle, Instagram } from "lucide-react"; // הוספנו את Instagram
 import styles from "./FloatingSocials.module.scss";
 
 const FloatingSocials = () => {
@@ -14,9 +14,19 @@ const FloatingSocials = () => {
                 <MessageCircle size={22} strokeWidth={1.8} />
             </a>
 
+            {/* כפתור אינסטגרם חדש */}
             <a
-                href="https://facebook.com/PLACEHOLDER_PAGE"
+                href="https://www.instagram.com/chabad_yaffo" // שנה ליוזר שלך
                 target="_blank"
+                rel="noopener noreferrer"
+                className={`${styles.fab} ${styles.instagram}`}
+                aria-label="מעבר לעמוד אינסטגרם"
+            >
+                <Instagram size={22} strokeWidth={1.8} />
+            </a>
+
+            <a
+                href="https://www.facebook.com/profile.php?id=61551949830199" target="_blank"
                 rel="noopener noreferrer"
                 className={`${styles.fab} ${styles.facebook}`}
                 aria-label="מעבר לעמוד פייסבוק"
