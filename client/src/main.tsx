ca401a850c516f91be0824f99a789574398d2ea2
TamamTamar <tamar.tamam770@gmail.com>
2026-06-11 12:03:30 +0300

    Optimize font loading

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./app/router";
 
+import "@fontsource/assistant/400.css";
+import "@fontsource/assistant/500.css";
+import "@fontsource/assistant/700.css";
+import "@fontsource/assistant/800.css";
+
+import { router } from "./app/router";
 import FloatingSocials from "./components/FloatingSocials/FloatingSocials";
 import "./styles/_base.scss";
 
